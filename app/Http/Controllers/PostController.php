72408d9f7d673b4ae525c347721441248db02962
EC2 Default User <ykshushoku@gmail.com>
2023-02-08 14:24:51 +0900

    作成画面表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,21 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
+<<<<<<< HEAD
         return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);
+=======
+        return view('posts/index')->with(['posts' => $post->getPaginateBylimit(1)]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' =>$post]);
+    }
+    
+    public function  create() 
+    {
+        return view('posts/create');    
+>>>>>>> 8cf6ac9 (作成画面表示)
     }
 }
 ?>
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -12,7 +12,9 @@
         </h1>
         <div class ='content'>
                     <div class ='content_post'>
-                        <h3>本部</h3>
+
+                        <h3>本文</h3>
+
                         <p class = 'body'>{{ $post->body }}</p>
                     </div>
         </div>
